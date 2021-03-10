ea96b4c91e5f44cda1746a460c5e0cbc61933995
CodexpathCommunity <marissav434@gmail.com>
2021-03-10 10:35:45 +0100

    4 binaries uploaded

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import PlanetOne from "../images/"
 
 const Section = styled.section`
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,18 +13,55 @@ const Section = styled.section`
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  height: 100vh;
+  height: 100%;
   padding: 3rem calc((100vw - 1300px) / 2);
 
   @media screen and (max-width: 768px) {
     grid-template-columns: 1fr;
   }
 `;
+
+const ColumnLeft = styled.div`
+  display: flex;
+  color: #fff;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 5rem 2rem;
+
+  h1 {
+    margin-bottom: 0.5rem;
+    font-size: 2rem;
+  }
+
+  p {
+    margin: 2em 0;
+    line-height: 1.1;
+    font-size: 4rem;
+  }
+`;
+
+const Button = styled.div`
+  padding: 1rem 3rem;
+  font-size: 1rem;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  background: transparent;
+`;
+const ColumnRight = styled.div``;
+
 const Hero = () => {
   return (
     <Section>
       <Container>
-        
+        <ColumnLeft>
+          <h1>Welcome to Mars</h1>
+          <p>Journey to the unknown</p>
+          <Button>Get Started</Button>
+        </ColumnLeft>
+        <ColumnRight>Filler</ColumnRight>
       </Container>
     </Section>
   );
